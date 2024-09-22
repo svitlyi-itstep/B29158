f32e6dee62db722994d3ae749f5aecb489983c13
svitlyi <svitlyi_a@itstep.academy>
2024-09-22 13:52:00 +0300

    Upgraded attack system

--- Lesson_2/character.py
+++ Lesson_2/character.py
@@ -11,9 +11,18 @@ class Character:
         self.defence = defence
 
     def show_stats(self):
-        print(
-            f' -< {self.name} >-\n'
-            f' Здоров`я: {self.health}\n'
-            f' Шкода: {self.damage}\n'
-            f' Захист: {self.defence}\n'
-        )
+        print(self)
+
+    def __str__(self):
+        return f' -< {self.name} >-\n' \
+               f' Здоров`я: {self.health}\n'\
+               f' Шкода: {self.damage}\n'\
+               f' Захист: {self.defence}\n'
+
+    def take_damage(self, damage):
+        real_damage = round(damage * ((100 - self.defence) / 100), 2)
+        self.health = round(max(self.health - real_damage, 0), 2)
+        return real_damage
+
+    def attack(self, target):
+        return target.take_damage(self.damage)
--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -1,13 +1,33 @@
 from character import Character
 
-player1 = Character('Vasya', 100, 3, 0)
+
+def attack_message(attacker: Character, target: Character, damage_done: float) :
+    return f'{attacker.name} атакував {target.name}.\n' \
+           f'{attacker.name} наніс {damage_done} шкоди. ' \
+           f'У {target.name} залишилося {target.health} здоров`я.'
+
+
+player1 = Character('Vasya', 100, 20, 20)
+player2 = Character('Petya', 100, 20, 10)
 
 print(f'Створено нового персонажа: {player1.name}')
+print(f'Створено нового персонажа: {player2.name}')
 # print(f'У {player1.name} {player1.health} здоров\'я.')
 player1.show_stats()
+player2.show_stats()
 
-incoming_damage = 20
-print(f'На {player1.name} напали бандити і нанесли {incoming_damage} шкоди.')
-player1.health -= incoming_damage
-print(f'Тепер у {player1.name} {player1.health} здоров\'я.')
-player1.show_stats()
+while player1.health > 0 and player2.health > 0:
+    damage_done = player1.attack(player2)
+    print(attack_message(player1, player2, damage_done))
+
+    damage_done = player2.attack(player1)
+    print(attack_message(player2, player1, damage_done))
+
+    print(f'{player1}\n{player2}')
+
+'''
+    Додати код, при якому player2 буде атакувати player1 у відповідь.
+    Продождувати атаки по черзі до тих пір, доки у одного з персонажів
+    не закінчиться здоров`я.
+
+'''
