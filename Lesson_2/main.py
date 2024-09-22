d31635d0675b7c5946e0ac7b2e81ef1371ed6883
svitlyi <svitlyi_a@itstep.academy>
2024-09-22 13:55:38 +0300

    Removed comments

--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -7,12 +7,12 @@ def attack_message(attacker: Character, target: Character, damage_done: float) :
            f'У {target.name} залишилося {target.health} здоров`я.'
 
 
-player1 = Character('Vasya', 100, 20, 20)
+player1 = Character('Vasya', 100, 20, 150)
 player2 = Character('Petya', 100, 20, 10)
 
 print(f'Створено нового персонажа: {player1.name}')
 print(f'Створено нового персонажа: {player2.name}')
-# print(f'У {player1.name} {player1.health} здоров\'я.')
+
 player1.show_stats()
 player2.show_stats()
 
@@ -24,10 +24,3 @@ while player1.health > 0 and player2.health > 0:
     print(attack_message(player2, player1, damage_done))
 
     print(f'{player1}\n{player2}')
-
-'''
-    Додати код, при якому player2 буде атакувати player1 у відповідь.
-    Продождувати атаки по черзі до тих пір, доки у одного з персонажів
-    не закінчиться здоров`я.
-
-'''
